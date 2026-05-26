2e6ef0867859b90aca9e31aa62c6ff38c16b5494
Martin-git-byte <mleonardocontrera@gmail.com>
2026-05-26 21:03:15 +0200

    Enable login requirement for Supabase configuration

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 window.SKILLS_HUB_SUPABASE = {
   url: "https://jlgzfemnphxiclqmterl.supabase.co",
   anonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9...04qie4iAX9bDkDR4zxLYfA_6S7JtGC653R7rWkfjQP0",
-  requireLogin: false,
+  requireLogin: true,
 };
