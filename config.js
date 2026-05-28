f50b78e6f9494b78de3074e8e7c6764bc371e56c
Martin-git-byte <mleonardocontrera@gmail.com>
2026-05-28 11:41:08 +0200

    Update Supabase anonKey in config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 window.SKILLS_HUB_SUPABASE = {
   url: "https://jlgzfemnphxiclqmterl.supabase.co",
-  anonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9...04qie4iAX9bDkDR4zxLYfA_6S7JtGC653R7rWkfjQP0",
+  anonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImpsZ3pmZW1ucGh4aWNscW10ZXJsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzk4MDY4NzAsImV4cCI6MjA5NTM4Mjg3MH0.04qie4iAX9bDkDR4zxLYfA_6S7JtGC653R7rWkfjQP0",
   requireLogin: true,
 };
